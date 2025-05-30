8f11e66c27325a983a755e78eef6d0739a88c03e
Subash B <subash59245@gmail.com>
2025-05-30 19:22:57 +0530

    Update run.py

--- run.py
+++ run.py
@@ -723,5 +723,8 @@ def internal_error(error):
     db.session.rollback()
     return render_template('error.html', error='Internal server error'), 500
 
-with app.app_context():
-    db.create_all()
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    port = 5000
+    app.run(host='0.0.0.0', port=port)
